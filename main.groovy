9c38c85f90d8f6389537d1f7291fe8fa8d6f6b39
Manikandan S <manikandan.subramanian@rapyder.com>
2024-04-26 11:19:21 +0530

    test repo added

--- main.groovy
+++ main.groovy
@@ -2,32 +2,50 @@
 createJobs()
 
 void createJobs() {
-    pipelineJob.with {
-
-        description("Test Groovy pipeline")
-
+    pipelineJob('job1') {
         definition {
-            cps{
-                script("""
-pipeline {
-
-    agent any
-    
-    options {
-        ansiColor('vga')
-        preserveStashes()
-    }
-
-
-    stages {
-        stage('Checks') {
-            steps{
-                sh "echo Hello World"
+            cpsScm {
+                scm {
+                    git {
+                        remote {
+                            url('https://github.com/jenkinsci/job-dsl-plugin.git')
+                        }
+                        branch('*/master')
+                    }
+                }
+                lightweight()
             }
         }
-           
-}""")
-                sandbox()
-            }}}
-
+    }
 }
+
+//void createJobs() {
+//    pipelineJob.with {
+//
+//        description("Test Groovy pipeline")
+//
+//        definition {
+//            cps{
+//                script("""
+//pipeline {
+//
+//    agent any
+//
+//    options {
+//        ansiColor('vga')
+//        preserveStashes()
+//    }
+//
+//
+//    stages {
+//        stage('Checks') {
+//            steps{
+//                sh "echo Hello World"
+//            }
+//        }
+//
+//}""")
+//                sandbox()
+//            }}}
+//
+//}
